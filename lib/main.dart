2b7d347b6d493ddc1a65307555fba259a14aa425
sanjulapro <desanjula99@gmail.com>
2020-05-14 19:07:47 +0530

    added stateless and statefull widget for random selector

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[600],
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Ask Me Everything',
+          style: TextStyle(fontSize: 24.0,fontFamily: 'Balsamiq'),
+        ),
+        backgroundColor: Colors.blue[600],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+
+  int ballNumber = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
